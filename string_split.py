22ec2f830f946bf69ba0c911998e3f242b4cf739
bromero26 <romero.bernardo@gmail.com>
2022-03-08 22:58:19 +0100

    added example of string.split()

---
+++ string_split.py
@@ -0,0 +1,8 @@
+# splitting a string "literal" and then printing the result
+split_world = "Hello World!".split()
+print(split_world)
+
+# assigning a string to a variable
+# then printing the result of calling the `split()` method on it
+world_msg = "Hello World!"
+print(world_msg.split())
